82a68190c49c48db91840bc631ea4d0e3ff38e5b
NiavlysLossam <sylvain.massol@gmail.com>
2026-02-20 19:11:07 +0100

    refactor: Rename Django project from geogalup to hierici.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# GéoGalup
+# HierIci
 Application créée dans une optique d'aprentissage. Application degéolocalisation de photographies anciennes.
--- hierici/urls.py
+++ hierici/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for geogalup project.
+URL configuration for hierici project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
